e2c3d5ba60ec7cbdaa9d26b5c577d467ee4b9f92
łukasz <96598800+rzadluka@users.noreply.github.com>
2023-03-04 22:22:35 -0600

    second request doesnt work

--- Madhacks.py
+++ Madhacks.py
@@ -1,4 +1,5 @@
 from canvasapi import Canvas
+import json
 import requests
 import os
 
@@ -19,9 +20,32 @@ for course in courses:
         except AttributeError:
             pass
 
+# REQUEST 1 --------------------
 course1 = canvas.get_course(6327379)
 assignment1 = course1.get_assignment(35995341)
 documentName = "test.pdf"
-command = "curl https://canvas.instructure.com/api/v1/courses/{0}/assignments/{1}/submissions/self/files -F 'name={2}' -F 'size={3}' -F 'parent_folder_path={4}' -H \"Authorization: Bearer {5}\"".format(course.id, assignments[0].id, documentName, os.path.getsize(documentName), "/", API_KEY)
-# print(command)
-os.system(command)
+url = 'https://canvas.instructure.com/api/v1/courses/{0}/assignments/{1}/submissions/self/files'.format(course.id, assignments[0].id)
+headers = {'Authorization': 'Bearer {0}'.format(API_KEY)}
+data = {'name': '{0}'.format(documentName),
+        'size': os.path.getsize(documentName),
+        'content_type': 'application/pdf',
+        'parent_folder_path': '/'}
+
+response = requests.post(url, headers=headers, data=data)
+
+print(response.status_code)
+print(response.text)
+
+# REQUEST 2 --------------------
+upload_url = response.json()['upload_url'].strip()
+filename = response.json()['upload_params']['filename']
+content_type = response.json()['upload_params']['content_type']
+data2 = {'key': (None, documentName),
+         'filename': filename,
+         'content_type': content_type,
+         'file': open('test.pdf', 'rb')}
+
+response2 = requests.post(upload_url, data=data2)
+
+print(response2.status_code)
+print(response2.text)
