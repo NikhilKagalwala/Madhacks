c10402adf62961fdc541af1cf61f201ff2a38d05
zierl <mzierl@wisc.edu>
2023-03-04 22:48:46 -0600

    added alot more methods

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 from __future__ import print_function
 import pickle
 import os.path
+import io
 from tabulate import tabulate
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from googleapiclient.http import MediaFileUpload
+from googleapiclient.errors import HttpError
+from googleapiclient.http import MediaIoBaseDownload
+from google.oauth2.credentials import Credentials
+
 from apiclient import errors
+import requests
+from tqdm import tqdm
+
+CurrentDirectoryID = None
 
 # If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly',
+SCOPES = ['https://www.googleapis.com/auth/drive.metadata',
+          'https://www.googleapis.com/auth/drive',
           'https://www.googleapis.com/auth/drive.file']
+
 
 def get_gdrive_service():
     creds = None
+    global CurrentDirectoryID
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
@@ -32,118 +44,70 @@ def get_gdrive_service():
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)
     # return Google Drive API service
-    return build('drive', 'v3', credentials=creds)
+    service = build('drive', 'v3', credentials=creds)
+    if CurrentDirectoryID is None:
+        root_folder = service.files().get(fileId='root', fields='id').execute()
+        CurrentDirectoryID = root_folder['id']
+    return service
 
 
-# def update_file(service, file_id, new_title, new_description, new_mime_type,
-#                 new_filename, new_revision):
-#   """Update an existing file's metadata and content.
-
-#   Args:
-#     service: Drive API service instance.
-#     file_id: ID of the file to update.
-#     new_title: New title for the file.
-#     new_description: New description for the file.
-#     new_mime_type: New MIME type for the file.
-#     new_filename: Filename of the new content to upload.
-#     new_revision: Whether or not to create a new revision for this file.
-#   Returns:
-#     Updated file metadata if successful, None otherwise.
-#   """
-#   try:
-#     # First retrieve the file from the API.
-#     file = service.files().get(fileId=file_id).execute()
-
-#     # File's new metadata.
-#     file['title'] = new_title
-#     file['description'] = new_description
-#     file['mimeType'] = new_mime_type
-
-#     # File's new content.
-#     media_body = MediaFileUpload(
-#         new_filename, mimetype=new_mime_type, resumable=True)
-
-#     # Send the request to the API.
-#     updated_file = service.files().update(
-#         fileId=file_id,
-#         body=file,
-#         newRevision=new_revision,
-#         media_body=media_body).execute()
-#     return updated_file
-#   except errors.HttpError, error:
-#     print 'An error occurred: %s' % error
-#     return None
-
-
-def upload_files():
-    """
-    Creates a folder and upload a file to it
-    """
+def upload_files(name):
     # authenticate account
     service = get_gdrive_service()
-    # folder details we want to make
-    folder_metadata = {
-        "name": "TestFolder", # Can input name of new folder HERE
-        "mimeType": "application/vnd.google-apps.folder" # Specifies type??
-    }
-    # create the folder
-    file = service.files().create(body=folder_metadata, fields="id").execute()
-    # get the folder id
-    folder_id = file.get("id")
-    print("Folder ID:", folder_id)
-    # upload a file text file
-    # first, define file metadata, such as the name and the parent folder ID
+    
+    #Get current directory ID
+    root_folder = service.files().get(fileId='root', fields='id').execute()
+    folder_id = root_folder['id']
+    
+    
     file_metadata = {
-        "name": "test.txt",
+        "name": name,
         "parents": [folder_id]
     }
     # upload
-    media = MediaFileUpload("test.txt", resumable=True)
-    file = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
-    print("File created, id:", file.get("id"))
+    media = MediaFileUpload(name, resumable=True) #May need file path
+    service.files().create(body=file_metadata, media_body=media, fields='id').execute()
     
-
-def search(service, query):
-    # search for the file
-    result = []
-    page_token = None
-    while True:
-        response = service.files().list(q=query,
-                                        spaces="drive",
-                                        fields="nextPageToken, files(id, name, mimeType)",
-                                        pageToken=page_token).execute()
-        # iterate over filtered files
-        for file in response.get("files", []):
-            result.append((file["id"], file["name"], file["mimeType"]))
-        page_token = response.get('nextPageToken', None)
-        if not page_token:
-            # no more files
-            break
-    return result
+def createDoc(name):
+    service = get_gdrive_service()
+    global CurrentDirectoryID
+    
+    file_metadata = {
+    'name': name,
+    'parents': [CurrentDirectoryID],
+    'mimeType': 'application/vnd.google-apps.document'
+    } 
+    file = service.files().create(body=file_metadata).execute()
+    print(f'File ID: {file.get("id")}')  
     
 
 
 def showCurrentDirectory():
     
     service = get_gdrive_service()
-    root_folder = service.files().get(fileId='root', fields='name').execute()
-    print('Current directory:', root_folder['name'])
-    # List all files and folders in the current directory
-    results = service.files().list(q="'root' in parents and trashed = false", fields="nextPageToken, files(id, name, mimeType)").execute()
-    items = results.get('files', [])
-    if not items:
-        print('No files or folders found.')
-    else:
-        print('Files and folders:')
-    for item in items:
-        print('{0} ({1})'.format(item['name'], item['mimeType']))
+    global CurrentDirectoryID
+    query = "'{}' in parents".format(CurrentDirectoryID)
+
+    try:
+        # retrieve all files in the folder using the files().list() method
+        results = service.files().list(q="parents='"+CurrentDirectoryID+"' and trashed=false",fields="nextPageToken, files(id, name, mimeType)").execute()
+        items = results.get('files', [])
+        
+        # print file information for each file in the folder
+        for item in items:
+            print(f'NAME: {item["name"]} ---------> TYPE: {item["mimeType"]}')
+            
+    except HttpError as error:
+        print(f'An error occurred: {error}')
     
 def createDirectory(name):
     # authenticate account
+    global CurrentDirectoryID
     service = get_gdrive_service()
     # folder details we want to make
     folder_metadata = {
         "name": name,
+        'parents': [CurrentDirectoryID],
         "mimeType": "application/vnd.google-apps.folder" # Specifies type
     }
     # create the folder
@@ -151,5 +115,61 @@ def createDirectory(name):
     return
 
 
+def query(FILE_NAME):
+    # Build the Drive API client
+    service = get_gdrive_service()
+    global CurrentDirectoryID
+
+    # Search for the file by name
+    if ".txt" in FILE_NAME:
+        query = "name='{}' and mimeType='text/plain' and trashed = false and parents='{}'".format(FILE_NAME, CurrentDirectoryID)
+    else:
+        query = "name='{}' and mimeType='application/vnd.google-apps.document' and trashed = false and parents='{}'".format("YO", CurrentDirectoryID)
+     
+    response = service.files().list(q=query, fields='nextPageToken, files(id, name)').execute()
+
+    # Check if a file was found
+    files = response.get('files', [])
+    if not files:
+        print('File not found in current directory')
+        return None
+    else:
+        # Print the ID of the first file found
+        return files[0]['id']
+
+
+def export_pdf(nameDoc):    
+    file = query(nameDoc)
+    if (file is None):
+        return
+    try:
+        service = get_gdrive_service()
+        request = service.files().export_media(fileId=file, mimeType='application/pdf')
+        file = io.BytesIO()
+        downloader = MediaIoBaseDownload(file, request)
+        done = False
+        while done is False:
+            status, done = downloader.next_chunk()
+            print(f"Download {int(status.progress() * 100)}.")
+        with open(nameDoc + '.pdf', 'wb') as f:
+            f.write(file.getbuffer())
+        print('Download Complete!')
+    except HttpError as error:
+        print(f'An error occurred: {error}')
+        
+def setDirectory(name):
+    global CurrentDirectoryID
+    service = get_gdrive_service()
+    query = "mimeType='application/vnd.google-apps.folder' and trashed=false and name='%s' and '%s' in parents" % (name, CurrentDirectoryID)
+    results = service.files().list(q=query, fields="nextPageToken, files(id, name)").execute()
+    items = results.get('files', [])
+
+    if not items:
+        print('Folder not found')
+        return
+    else:
+        CurrentDirectoryID = items[0]['id']
+
 if __name__ == '__main__':
+    setDirectory("DNE")
     showCurrentDirectory()
