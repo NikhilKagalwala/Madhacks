ad98a19a4bd467c3610bcac959abc4ce56683027
łukasz <96598800+rzadluka@users.noreply.github.com>
2023-03-04 23:32:41 -0600

    code now submits assignment test.pdf

--- Madhacks.py
+++ Madhacks.py
@@ -1,12 +1,12 @@
 from canvasapi import Canvas
-import json
 import requests
 import os
 
 API_URL = "https://canvas.instructure.com/"
 API_KEY = "7~ZLz5HcFc2G5nJT5GCswngqzHx3gixHL0tjCQoD4CNlLQIDIQH3mXa6lIkdAeuhXU"
+USER_ID = 37896736
 canvas = Canvas(API_URL, API_KEY)
-user = canvas.get_user(37896736)
+user = canvas.get_user(USER_ID)
 courses = canvas.get_courses()
 
 for course in courses:
@@ -24,28 +24,39 @@ for course in courses:
 course1 = canvas.get_course(6327379)
 assignment1 = course1.get_assignment(35995341)
 documentName = "test.pdf"
-url = 'https://canvas.instructure.com/api/v1/courses/{0}/assignments/{1}/submissions/self/files'.format(course.id, assignments[0].id)
-headers = {'Authorization': 'Bearer {0}'.format(API_KEY)}
-data = {'name': '{0}'.format(documentName),
+content_type = "application/pdf"
+url = f'https://canvas.instructure.com/api/v1/courses/{course1.id}/assignments/{assignment1.id}/submissions/{USER_ID}/files'
+headers = {'Authorization': f'Bearer {API_KEY}'}
+data = {'name': f'{documentName}',
         'size': os.path.getsize(documentName),
-        'content_type': 'application/pdf',
+        'content_type': f'{content_type}',
         'parent_folder_path': '/'}
-
 response = requests.post(url, headers=headers, data=data)
+# print(response.status_code)
+# print(response.text)
 
-print(response.status_code)
-print(response.text)
 
 # REQUEST 2 --------------------
-upload_url = response.json()['upload_url'].strip()
-filename = response.json()['upload_params']['filename']
-content_type = response.json()['upload_params']['content_type']
-data2 = {'key': (None, documentName),
-         'filename': filename,
-         'content_type': content_type,
-         'file': open('test.pdf', 'rb')}
+upload_url = response.json()["upload_url"]
+upload_params = response.json()["upload_params"]
 
-response2 = requests.post(upload_url, data=data2)
+# 3. Upload file to Canvas
+with open(documentName, "rb") as f:
+    files = {'file': (documentName, f, f'{content_type}')}
+    response = requests.post(upload_url, data=upload_params, files=files)
+# print(response.status_code)
+# print(response.text)
 
-print(response2.status_code)
-print(response2.text)
+# 4. Follow the redirect to complete upload
+redirect_url = response.headers['Location']
+print(redirect_url)
+response = requests.get(redirect_url, headers=headers)
+# print(response.status_code)
+# print(response.text)
+
+
+# REQUEST 3 -------------------- Submitting the assignment
+url = f'https://canvas.instructure.com/api/v1/courses/{course1.id}/assignments/{assignment1.id}/submissions'
+response = requests.post(url, headers=headers, data={'submission[submission_type]': 'online_upload', 'submission[file_ids][]': f'{response.json()["id"]}'})
+print(response.status_code)
+# print(response.text)
